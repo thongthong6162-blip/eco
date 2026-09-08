0109a9c97eed3941953dda1728f094f604012d00
thongthong6162-blip <thongthong6162@gmail.com>
2026-09-08 16:41:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,18 +16,11 @@
 #!/bin/sh
 set -e
 echo "Starting Laravel..."
-
 # Create SQLite DB if missing
 touch database/database.sqlite
-
 # Run migrations
 php artisan migrate --force
-
-# Seed only if not already seeded
-if [ ! -f database/.seeded ]; then
-  php artisan db:seed --force
-  touch database/.seeded
-fi
-
+# Seed the database
+php artisan db:seed --force
 # Start PHP server
 php -S 0.0.0.0:10000 -t public
